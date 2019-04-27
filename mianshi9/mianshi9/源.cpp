4a51ed6c0339a84b08b89b8018584284da48553f
zhangkai <871227769@qq.com>
2019-04-27 15:43:27 +0800

    Update 源.cpp

--- mianshi9/mianshi9/源.cpp
+++ mianshi9/mianshi9/源.cpp
@@ -1,6 +1,9 @@
+// 面试题9：用两个栈实现队列
+// 题目：用两个栈实现一个队列。队列的声明如下，请实现它的两个函数appendTail
+// 和deleteHead，分别完成在队列尾部插入结点和在队列头部删除结点的功能。
 #include"Queue.h"
 
-void Test(char actual,CQueue<char>& test)//������Ҫ���ã���Ȼ�����Ķ������޷�������������в�����
+void Test(char actual,CQueue<char>& test)//类这里要引用，不然拷贝的东西是无法对类对象本身进行操作的
 {
 	try
 	{
