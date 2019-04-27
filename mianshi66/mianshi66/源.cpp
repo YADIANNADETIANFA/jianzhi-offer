0f52e84277d369d991c062d95f77667029301230
zhangkai <871227769@qq.com>
2019-04-27 15:41:27 +0800

    Update 源.cpp

--- mianshi66/mianshi66/源.cpp
+++ mianshi66/mianshi66/源.cpp
@@ -1,9 +1,12 @@
+// 面试题66：构建乘积数组
+// 题目：给定一个数组A[0, 1, …, n-1]，请构建一个数组B[0, 1, …, n-1]，其
+// 中B中的元素B[i] =A[0]×A[1]×… ×A[i-1]×A[i+1]×…×A[n-1]。不能使用除法。
 #include<cstdio>
 #include<iostream>
 #include<vector>
 using namespace std;
 
-int main()//����A�����������0�ĳ��֣�������Խ�һ���Ż���ʹ����������
+int main()//考虑A数组里面会有0的出现，代码可以进一步优化，使计算量更少
 {
 	int n=0;
 	while (cin >> n)
