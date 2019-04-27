61e1475c82337adb3c1457d8e28e55a4613b0d9e
zhangkai <871227769@qq.com>
2019-04-27 15:22:24 +0800

    Update 源.cpp

--- mianshi46_1/mianshi46_1/源.cpp
+++ mianshi46_1/mianshi46_1/源.cpp
@@ -1,6 +1,11 @@
-//ѭ�����ҵ���ʵ�֣�û���ظ��ķ���
+// 面试题46：把数字翻译成字符串
+// 题目：给定一个数字，我们按照如下规则把它翻译为字符串：0翻译成"a"，1翻
+// 译成"b"，……，11翻译成"l"，……，25翻译成"z"。一个数字可能有多个翻译。例
+// 如12258有5种不同的翻译，它们分别是"bccfi"、"bwfi"、"bczi"、"mcfi"和
+// "mzi"。请编程实现一个函数用来计算一个数字有多少种不同的翻译方法。
+//循环从右到左实现，没有重复的翻译
 #include<cstdio>
-#include<string>//ͷ��û��c��
+#include<string>//头是没有c的
 
 using namespace std;
 
@@ -24,19 +29,19 @@ int NumOfTranslation(const string& numTostr)
 		if (i == length - 1)
 			count = 1;
 		else
-			count = counts[i + 1];//��������Ԫ���ܷ��ϣ��������в���ϵ�Ȩ����Ҳ����˵����ֵΪcounts[i + 1]��Ҫ���ܽ�Ͼ��ٶ����
+			count = counts[i + 1];//不管两个元素能否结合，他俩都有不结合的权力，也就是说基础值为counts[i + 1]，要是能结合就再额外加
 
 		if (i < length - 1)
 		{
 			if (i == length - 2)
 			{
 				if ((numTostr[i] - '0') * 10 + (numTostr[i + 1] - '0') <= 25 && (numTostr[i] - '0') != 0)
-					count += 1;//����ӵ�
+					count += 1;//额外加的
 			}
 			else if ((numTostr[i] - '0') * 10 + (numTostr[i + 1] - '0') <= 25 && (numTostr[i] - '0') != 0)
-				count += counts[i + 2];//����ӵ�
+				count += counts[i + 2];//额外加的
 		}
-		counts[i] = count;//���ж�counts[]��ÿһ��Ԫ�ظ�ֵ
+		counts[i] = count;//集中对counts[]的每一个元素赋值
 	}
 	count = counts[0];
 	delete[] counts;
