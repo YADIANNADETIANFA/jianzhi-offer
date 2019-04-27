a68a62df3b562d1b9fbe25df3e29bcdaa925763c
zhangkai <871227769@qq.com>
2019-04-27 15:39:57 +0800

    Update 源.cpp

--- mianshi64_2/mianshi64_2/源.cpp
+++ mianshi64_2/mianshi64_2/源.cpp
@@ -1,4 +1,7 @@
-//�����麯��
+// 面试题64：求1+2+…+n
+// 题目：求1+2+…+n，要求不能使用乘除法、for、while、if、else、switch、case
+// 等关键字及条件判断语句（A?B:C）。
+//利用虚函数
 #include<cstdio>
 #include<iostream>
 using namespace std;
@@ -18,7 +21,7 @@ public:
 class B:public A
 {
 public:
-	virtual unsigned int Sum(int n)//�麯��Ҫ���������������ͣ�������������������ͬ���������Բ���A����virtual unsigned int Sum(int n)����B����virtual unsigned int Sum(unsigned int n),�����Ļ�������ֶ�̬���ˣ�ֻ��һֱִ��A��
+	virtual unsigned int Sum(int n)//虚函数要两个函数返回类型，函数名，参数类型相同。。。所以不能A中是virtual unsigned int Sum(int n)；而B中是virtual unsigned int Sum(unsigned int n),这样的话不会出现多态性了！只会一直执行A的
 	{
 		return Array[!!n]->Sum(n - 1) + n;
 	}
