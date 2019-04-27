d71702d6b04c901a8e14359daac90224cbd0e44f
zhangkai <871227769@qq.com>
2019-04-27 14:53:55 +0800

    Update 源.cpp

--- mianshi14/mianshi14/源.cpp
+++ mianshi14/mianshi14/源.cpp
@@ -1,3 +1,8 @@
+// 面试题14：剪绳子
+// 题目：给你一根长度为n绳子，请把绳子剪成m段（m、n都是整数，n>1并且m≥1）。
+// 每段的绳子的长度记为k[0]、k[1]、……、k[m]。k[0]*k[1]*…*k[m]可能的最大乘
+// 积是多少？例如当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此
+// 时得到最大的乘积18。
 #include<cstdio>
 #include<cmath>
 #include<iostream>
@@ -47,7 +52,7 @@ int maxproductAftercutting2(int length)
 		return 4;
 
 	int timeOfcut = 0;
-	/*while (length >=5)//����whileѭ�����˷�����
+	/*while (length >=5)//²»ÓÃwhileÑ­»·£¬ÀË·ÑÐÔÄÜ
 	{
 		++timeOfcut;
 		length -= 3;
