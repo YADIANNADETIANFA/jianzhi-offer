6b5dffc63bca0c7cc223eaffccdabe2dc7deedb8
zhangkai <871227769@qq.com>
2019-04-29 09:37:44 +0800

    Update 源.cpp

--- mianshi62_3/mianshi62_3/源.cpp
+++ mianshi62_3/mianshi62_3/源.cpp
@@ -1,3 +1,6 @@
+// 面试题62：圆圈中最后剩下的数字
+// 题目：0, 1, …, n-1这n个数字排成一个圆圈，从数字0开始每次从这个圆圈里
+// 删除第m个数字。求出这个圆圈里剩下的最后一个数字。
 #include<iostream>
 #include<cstdio>
 using namespace std;
@@ -9,7 +12,7 @@ int main()
 	{
 		if (n < 1 || m < 1)
 		{
-			cout << -1 << endl;//����Ҫдcout<<-1<<endl;  Ҫ��return -1�Ļ���������whileѭ���ˣ�cin��û����
+			cout << -1 << endl;//这里要写cout<<-1<<endl;  要是return -1的话，就跳出while循环了，cin就没用了
 			continue;
 		}
 		int last = 0;
